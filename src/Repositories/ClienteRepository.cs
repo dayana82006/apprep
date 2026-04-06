1b0cf2ca0fdde779352ed0742401603ec75cd21a
Sistemas 17 <dhernandez@grupoapl.net>
2026-04-06 11:42:09 -0500

    Mejora de consulta SQL en ClienteRepository
    Se actualiza la consulta SQL para obtener clientes, incorporando JOINs con Clientes y PersonasEmpresas, nuevos campos en el SELECT y filtrado por clientes activos. Se ajusta la lógica del campo dv y se mejora la estructura de los datos retornados.

--- src/Repositories/ClienteRepository.cs
+++ src/Repositories/ClienteRepository.cs
@@ -21,17 +21,32 @@ public class ClienteRepository : IClienteRepository
                             DECLARE @PageNumber INT = @numpag;  -- Número de página que quieres
                             DECLARE @RowsPerPage INT = 500; -- Cantidad de filas por página
 
-                                SELECT 
-                                    NoIdentificacion AS codigo,
-                                    DigitoDeChequeo AS dv,
-                                    '' AS sucursal,
-                                    RazonSocial AS razonsocial,
-                                    NombreCompleto AS nombrecontacto,
-                                    Direccion AS direccion,
-                                    Telefono1 AS telefono,
-                                    Email AS email,
-                                    1 AS estado
-                                FROM Personas
+                               SELECT 
+                                NoIdentificacion AS codigo,
+                                CASE WHEN  T.TipoDeDocumentoId = 2 THEN  DigitoDeChequeo ELSE '' END  AS dv,
+                                '' AS sucursal,
+                                NombreCompleto AS razonsocial,
+                                NombreCompleto AS nombrecontacto,
+                                Direccion AS direccion,
+                                Telefono1 AS telefono,
+                                '' AS codlistaprecio,
+                                '' AS condicionpago,
+                                '' AS periodicidad,
+                                T1.VendedorPOSId AS codVendedor,
+                                T2.CupoNeto AS cupo,
+                                '' AS codigogrupodcto,
+                                Email AS email,
+                                '' AS tipocliente,
+                                '' AS tipotercero,
+                                1 AS estado
+                                FROM Personas T 
+                                INNER JOIN
+                                DBO.Clientes T1
+                                ON ( T.PersonaId= T1.PersonaId )
+                                INNER JOIN
+                                DBO.PersonasEmpresas T2
+                                ON ( T2.PersonaId= T.PersonaId )
+                                WHERE T1.Estado=1
                                 ORDER BY NoIdentificacion
                                 OFFSET (@PageNumber - 1) * @RowsPerPage ROWS
                                 FETCH NEXT @RowsPerPage ROWS ONLY";
